a242f0e7f7cccaca21e1739cfb3f40f5cf39fe95
Haruaki Tamada <tamada@users.noreply.github.com>
2025-02-18 20:23:31 +0900

    Refactor clone_and_find tests to use async/await syntax and improve clarity of HTTP request assertions

--- tests/clone_and_find.rs
+++ tests/clone_and_find.rs
@@ -2,8 +2,10 @@ use gixor::{Gixor, Name, Result};
 
 mod common;
 
-#[test]
-fn test_clone_and_find() -> Result<()> {
+/// send a request of HTTP GET to the content URL of the boilerplate,
+///  and this test assumes that the response will be 200 OK.
+#[tokio::test]
+async fn test_clone_and_find() -> Result<()> {
     let gixor = match common::setup() {
         Ok(gixor) => gixor,
         Err(e) => {
@@ -17,23 +19,10 @@ fn test_clone_and_find() -> Result<()> {
     assert_eq!(result.boilerplate_name(), "Rust".to_string());
     assert_eq!(result.repository_name(), "default");
 
-    Ok(())
-}
-
-/// send a request of HTTP GET to the content URL of the boilerplate,
-///  and this test assumes that the response will be 200 OK.
-#[tokio::test]
-async fn test_content_url() {
-    let gixor = match common::setup() {
-        Ok(gixor) => gixor,
-        Err(e) => {
-            panic!("Failed to initialize Gixor: {}", e);
-        }
-    };
-    let r1 = gixor.find(Name::parse("rust")).unwrap();
-    let url1 = r1.content_url().unwrap();
+    let url1 = result.content_url().unwrap();
     let resp = reqwest::get(url1).await.unwrap();
     assert_eq!(resp.status(), reqwest::StatusCode::OK);
+    Ok(())
 }
 
 #[test]
